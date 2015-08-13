20b49636020646009ece664a77df90650666a7a9
Ashish Maithani <ashish.maithani@outlook.com>
2015-08-12 22:17:59 -0400

    GUI changes to the game intro

--- Snake.py
+++ Snake.py
@@ -9,22 +9,24 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("Snake")
 
-myIcon = pygame.image.load("myIcon.png")
+myIcon = pygame.image.load('images/myIcon.png')
 pygame.display.set_icon(myIcon)
 
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 green = (0,150,0)
+lightGreen = (0,255,0)
 blue = (0,0,255)
-orange = (255,204,170)
+orange = (230,92,0)
+lightOrange = (255,165,5)
 
 block_size = 10
 
 clock = pygame.time.Clock()
 
-img = pygame.image.load("snake.png")
-ratImg = pygame.image.load("angryBird.png")
+img = pygame.image.load("images/snake.png")
+ratImg = pygame.image.load("images/angryBird.png")
 
 
 def displayScore(score):
@@ -57,7 +59,6 @@ def snake(block_size, snakeList, headDirection):
 
 
 def apple(randAppleX,randAppleY,block_size):
-    #gameDisplay.fill(red, rect=[randAppleX,randAppleY,block_size,block_size])
     gameDisplay.blit(ratImg, (randAppleX, randAppleY))
 
 
@@ -80,14 +81,35 @@ def pause():
 
             clock.tick(5)
 
+def button(msg, x, y, width, height, color, hoverColor):
+    mousePos =  pygame.mouse.get_pos()
+    mouseClick = pygame.mouse.get_pressed()
+    print mouseClick
+
+    if x <= mousePos[0] <= x+width and y <= mousePos[1] <= y+height:
+        pygame.draw.rect(gameDisplay, hoverColor, [x,y,width,height])
+        if mouseClick[0]== 1:
+            if msg == "Quit":
+                pygame.quit()
+                quit()
+            elif msg == "Play!!":
+                gameLoop()
+    else:
+        pygame.draw.rect(gameDisplay, color, [x,y,width,height])
+
+    myFont = pygame.font.SysFont("arial",25)
+    textSurface = myFont.render(msg, True, black)
+    textRect = textSurface.get_rect()
+    textRect.center = (x + width/2, y + height/2)
+    gameDisplay.blit(textSurface, textRect)
+
 
 def gameIntro():
     intro = True
+
     gameDisplay.fill(white)
     message("Welcome to the Snake Game!!",green,25,-50)
     message("The objective of the game is to eat Birds",black,25)
-    message("Press C to Play or Q to Quit.",black,20,100)
-    pygame.display.update()
 
     while intro:
         for event in pygame.event.get():
@@ -101,7 +123,11 @@ def gameIntro():
                 pygame.quit()
                 quit()
 
-            clock.tick(5)
+        button("Play!!", 200, 400, 100, 50, green, lightGreen)
+        button("Quit", 500, 400, 100, 50, orange, lightOrange)
+
+        pygame.display.update()
+        clock.tick(5)
 
 
 def gameLoop():
@@ -120,8 +146,8 @@ def gameLoop():
     fps = 10
     fps_change = 5
 
-    randAppleX = random.randrange(0,display_width-block_size)
-    randAppleY = random.randrange(0,display_height-block_size)
+    randAppleX = random.randrange(0,display_width-block_size,block_size)
+    randAppleY = random.randrange(0,display_height-block_size,block_size)
 
     snakeList = []
     snakeSize = 1
@@ -172,7 +198,6 @@ def gameLoop():
         lead_y = lead_y + change_y
 
         gameDisplay.fill(white)
-        #pygame.draw.rect(gameDisplay, red, [400,300,10,10])
         apple(randAppleX,randAppleY,block_size)
 
         snakeHead = []
@@ -200,9 +225,8 @@ def gameLoop():
                 if score_gap - score_inc < 0:
                     score_gap = 200
                     fps += fps_change
-                randAppleX = random.randrange(0,display_width-block_size)
-                randAppleY = random.randrange(0,display_height-block_size)
-
+                randAppleX = random.randrange(0,display_width-block_size,10)
+                randAppleY = random.randrange(0,display_height-block_size,10)
             else:
                 snakeList.pop(0)
         else:
@@ -212,5 +236,6 @@ def gameLoop():
     quit()
 
 
+##Call to the gameIntro.
+##GameIntro will in turn call gameLoop depending upon input.
 gameIntro()
-gameLoop()
